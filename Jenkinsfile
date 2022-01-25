5d96910c819112cb51169881aebdf987201136cb
AdriRameti <adri7agu@gmail.com>
2022-01-25 16:07:58 +0100

    first test

---
+++ Jenkinsfile
@@ -0,0 +1,28 @@
+pipeline {
+    agent any
+    triggers { 
+        pollSCM('0 */3 * * *') 
+    }
+    stages {
+        stage('Linter'){
+            script {
+                env.LINT = sh(script: "npm run lint",returnStatus:true)
+            }
+        }
+        stage('Test'){
+            script {
+                env.TEST = sh(script: " npm run build && npm start",returnStatus:true)
+            }
+        }
+        stage('Update_Readme'){
+            sh """
+                node index.js ${env.TEST}
+            """
+        }
+    }
+    parameters {
+        text(name:'Ejecutor', defaultValue:'''Nombre de la persona'''),
+        text(name:'Motivo', defaultValue:'''Motivo de ejecución'''),
+        text(name:'Correo', defaultValue:'''ejemplo@ejemplo.com''')
+    }
+}
---
+++ jenkinsScripts/index.js
@@ -0,0 +1,30 @@
+const core = require('@actions/core');
+const fs = require('fs');
+
+async function new_badge() {
+
+    let outcome = Boolean(process.argv[2]);
+    let readme = 'README.md'; 
+    let badge;
+    console.log(outcome, (outcome == 'success'));
+    if (outcome == true) {
+        console.log('Entra success');
+        badge = '![badge-success](https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg)'
+    } else {
+        badge = '![badge-failure](https://img.shields.io/badge/test-failure-red)'
+    }
+
+    fs.readFile(readme, 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        var result = data.replace(/(?<=\<!---Start place for the badge --\>\n)[^]+(?=\n\<!---End place for the badge --\>)/g, badge);
+
+        fs.writeFile(readme, result, 'utf8', function (err) {
+            if (err) return console.log(err);
+        });
+    });
+
+}
+
+new_badge();
