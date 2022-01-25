c48d5700c62fa76a9551748e9fdf5c507e61ddd8
AdriRameti <adri7agu@gmail.com>
2022-01-25 16:50:56 +0100

    update issues

--- jenkinsScripts/index.js
+++ jenkinsScripts/index.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 
 async function new_badge() {
 
-    let outcome = Boolean(process.argv[2]);
+    let outcome = !Boolean(process.argv[2]);
     let readme = '../README.md'; 
     let badge;
-    console.log(process.argv[2]);
+    console.log(outcome);
     if (outcome == true) {
         console.log('Entra success');
         badge = '![badge-success](https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg)'
