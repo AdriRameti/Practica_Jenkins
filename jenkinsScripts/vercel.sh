d66b77dcf321220e6abad1905e385e6ab98531c2
AdriRameti <adri7agu@gmail.com>
2022-01-31 17:29:14 +0100

    update vercel sh

--- jenkinsScripts/vercel.sh
+++ jenkinsScripts/vercel.sh
@@ -1,8 +1,7 @@
 if [ $1 -eq 0 ] && [ $2 -eq 0 ] && [ $3 -eq 0 ] && [ $4 -eq 0 ]
     then
-        echo $2
         vercel . --token $5 --confirm --name practica-jenkins
-        exit 0
+        exit $?
 else
     echo $2
     exit 1
